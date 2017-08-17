3a04372dd0ee33144d3faa0eba2e7c13da39aca5
kidevelop <hjh010501@naver.com>
2017-08-17 22:33:28 +0900

    Textblock bug fix, SettingWindow update

--- NeuralAction/NeuralAction.WPF/SettingWindow.xaml.cs
+++ NeuralAction/NeuralAction.WPF/SettingWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -19,9 +20,26 @@ namespace NeuralAction.WPF
     /// </summary>
     public partial class SettingWindow : Window
     {
+        int scale;
         public SettingWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+
+            scale = (int)PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice.M11;
+
+            this.Height = SystemParameters.WorkArea.Height;
+            this.Top = 0;
+            this.Left = (SystemParameters.WorkArea.Width) - this.Width;
+           
         }
     }
 }
